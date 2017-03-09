15becb602f3b55fbfccb9d8a392f557f2cb326a3
phoebuscy <phoebuscy@163.com>
2017-03-09 18:51:30 +0800

    能自动拷贝的mvn

---
+++ code/core/spy/src/com/phoebuscy/SpyMain.java
@@ -0,0 +1,22 @@
+package com.phoebuscy;
+
+/**
+ * Created by 10049992 on 2017/3/9.
+ */
+public class SpyMain
+{
+
+    public int getInt(int a)
+    {
+        if (a > 1)
+        {
+            return 100;
+        }
+        else
+        {
+            return 5;
+        }
+    }
+
+
+}

97e066ff85ac3b20f0f7eea9e7476c9e93d28e41
phoebuscy <phoebuscy@163.com>
2017-03-14 00:52:47 +0800

    修改国际化文件

--- code/core/spy/src/com/table/SOptionLinkTable.java
+++ code/core/spy/src/com/table/SOptionLinkTable.java
@@ -1,5 +1,6 @@
 package com.table;
 
+import com.TConst;
 import com.render.SRosePercentRender;
 import com.render.SRoserender;
 import com.table.tableModel.SOptionLinkTableModel;
@@ -7,6 +8,7 @@ import com.table.tableModel.SOptionLinkTableModel;
 import javax.swing.*;
 import java.util.Random;
 
+import static com.TFileUtil.getConfigValue;
 import static com.util.SUtil.setColumWidth;
 
 /**
@@ -36,20 +38,22 @@ public class SOptionLinkTable extends JTable
 
     private Object[][] getTableData()
     {
+       String callRaise = getConfigValue("call.raise", TConst.CONFIG_I18N_FILE); //CALL涨
+       String putDown = getConfigValue("put.down", TConst.CONFIG_I18N_FILE); //PUT跌
 
-        Object[][] data1 = {{2552.50, "CALL涨", 2.85, 3.2, 0.18, 2.89, 2.87, 5000, 23436},
-                            {255.50, "PUT跌", 2.85, -3.2, -0.18, 2.89, 2.87, 5000, 23436},
-                            {2561.00, "CALL涨", 2.85, 3.2, 0.18, 2.89, 2.87, 5000, 23436},
-                            {255.50, "PUT跌", 2.85, 3.2, 0.18, 2.89, 2.87, 5000, 23436},
-                            {2561.00, "CALL涨", 2.85, -1.2, -0.18, 2.89, 2.87, 5000, 23436},
-                            {2561.00, "CALL涨", 2.85, 3.2, 0.18, 2.89, 2.87, 5000, 23436},
-                            {255.50, "PUT跌", 2.85, -3.2, 0.18, 2.89, 2.87, 5000, 23436},
-                            {256.00, "PUT跌", 2.85, 3.2, 0.18, 2.89, 2.87, 5000, 23436}};
+        Object[][] data1 = {{2552.50, callRaise, 2.85, 3.2, 0.18, 2.89, 2.87, 5000, 23436},
+                            {255.50, putDown, 2.85, -3.2, -0.18, 2.89, 2.87, 5000, 23436},
+                            {2561.00, callRaise, 2.85, 3.2, 0.18, 2.89, 2.87, 5000, 23436},
+                            {255.50, putDown, 2.85, 3.2, 0.18, 2.89, 2.87, 5000, 23436},
+                            {2561.00, callRaise, 2.85, -1.2, -0.18, 2.89, 2.87, 5000, 23436},
+                            {2561.00, callRaise, 2.85, 3.2, 0.18, 2.89, 2.87, 5000, 23436},
+                            {255.50, putDown, 2.85, -3.2, 0.18, 2.89, 2.87, 5000, 23436},
+                            {256.00, putDown, 2.85, 3.2, 0.18, 2.89, 2.87, 5000, 23436}};
 
-        Object[][] data2 = {{2553.50, "CALL涨", 2.85, 3.2, 0.18, 2.89, 2.87, 5000, 23436},
-                            {2561.00, "CALL涨", 2.85, +3.2, +0.18, 2.89, 2.87, 5000, 23436},
-                            {255.50, "PUT跌", 2.85, 3.2, 0.18, 2.89, 2.87, 5000, 23436},
-                            {2565.02, "PUT跌", 2.85, -3.2, -0.18, 2.89, 2.87, 5000, 23436}};
+        Object[][] data2 = {{2553.50, callRaise, 2.85, 3.2, 0.18, 2.89, 2.87, 5000, 23436},
+                            {2561.00, callRaise, 2.85, +3.2, +0.18, 2.89, 2.87, 5000, 23436},
+                            {255.50, putDown, 2.85, 3.2, 0.18, 2.89, 2.87, 5000, 23436},
+                            {2565.02, putDown, 2.85, -3.2, -0.18, 2.89, 2.87, 5000, 23436}};
 
         Random random = new Random();
         boolean b = random.nextBoolean();

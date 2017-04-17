01aa32e6450f0b21a997d205c7154a8471c74654
phoebuscy <phoebuscy@163.com>
2017-04-18 00:16:32 +0800

    log4j2 例子完成

--- code/core/utils/src/example/loger/TLog4j2Test.java
+++ code/core/utils/src/example/loger/TLog4j2Test.java
@@ -8,12 +8,13 @@ import org.apache.logging.log4j.Logger;
  */
 public class TLog4j2Test
 {
-    private static Logger logger = LogManager.getLogger(TLog4j2Test.class.getName());
+    private static Logger logger = LogManager.getLogger("msglog");
+    private static Logger logger2 = LogManager.getLogger("");
 
     public static void main(String[] args)
     {
         int b = 1;
-        for(int i = 0; i < 100000; i++)
+        for(int i = 0; i < 1000; i++)
         {
             logger.trace("trace level");
             logger.debug("debug level");
@@ -21,6 +22,14 @@ public class TLog4j2Test
             logger.warn("warn level");
             logger.error("error level");
             logger.fatal("fatal level");
+
+
+            logger2.trace("trace level2");
+            logger2.debug("debug level2");
+            logger2.info("info level2");
+            logger2.warn("warn level2");
+            logger2.error("error level2");
+            logger2.fatal("fatal level2");
         }
         int a = 1;
     }
